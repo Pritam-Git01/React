094071e3a64313f69decc9c9f3f3d0c4ee72e50e
Pritam-Git01 <pritamyadav.ds.work.@gmail.com>
2023-04-27 02:07:00 +0530

    Day-06

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import {useState} from "react";
+import {useState} from 'react';
 import './App.css';
 
 function App() {
 
-  const[count,setCount] = useState(0)
+  const [name,setName] = useState(' ')
 
-  function add(){
-    setCount(count + 1)
+
+  function handleChange(event){
+    setName(event.target.value) 
   }
-  function subtract(){
-    setCount(count-1)
-    if(count === 0){
-      setCount(0)
-    }
+
+  function handleClick (){
+    setName()
   }
   return (
     <div className="App">
-    
-    <h2>Count is : {count}</h2>
-    <button className='first'onClick={add}>Increase</button>
-    <button className='second'onClick={subtract}>Decrease</button>
-       
+      
+
+
+      <input onChange={handleChange}placeholder="Add Your Name"/>
+    <button onClick={handleClick}>Submit</button>
+
+    <h2>My name is {name}</h2>
     </div>
   );
 }
