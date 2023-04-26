8ec2deea3ed585b19923465a110646e3dd6949cd
Pritam-Git01 <pritamyadav.ds.work.@gmail.com>
2023-04-27 01:08:02 +0530

    Day-4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 
 function App() {
+
+  const[count,setCount] = useState(0)
+
+  function add(){
+    setCount(count + 1)
+  }
+  function subtract(){
+    setCount(count-1)
+    if(count === 0){
+      setCount(0)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <h2>Count is : {count}</h2>
+    <button className='first'onClick={add}>Increase</button>
+    <button className='second'onClick={subtract}>Decrease</button>
+       
     </div>
   );
 }
