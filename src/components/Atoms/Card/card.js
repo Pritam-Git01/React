f2f84b1a81c94057610c42d6a3294f7b0920f2f3
Pritam-Git01 <pritamyadav.ds.work.@gmail.com>
2023-05-12 22:02:18 +0530

    do updation in scrollbar file

--- src/components/Atoms/Card/card.js
+++ src/components/Atoms/Card/card.js
@@ -7,6 +7,7 @@ export default function Card({UserProfile}){
         position,
         Exp
     } = UserProfile
+    
 
 
     return (
--- src/components/Molecule/ScrollBar/scrollbar.js
+++ src/components/Molecule/ScrollBar/scrollbar.js
@@ -8,14 +8,7 @@ export default function ScrollBar(){
 
     return (
         <div className={styles.wraper}>
-          <div className={styles.card}><Card  UserProfile={profile[0]} /></div>
-          <div className={styles.card}><Card  UserProfile={profile[1]} /></div>
-          <div className={styles.card}><Card  UserProfile={profile[2]} /></div>
-          <div className={styles.card}><Card  UserProfile={profile[3]} /></div>
-          <div className={styles.card}><Card  UserProfile={profile[4]} /></div>
-          <div className={styles.card}><Card  UserProfile={profile[5]} /></div>
-          <div className={styles.card}><Card  UserProfile={profile[6]} /></div>
-          <div className={styles.card}><Card  UserProfile={profile[7]} /></div>  
+          {profile.map((user) => <div className={styles.card}><Card  UserProfile={user} /></div>)}
         </div>
     )
 }
